70b80145376ef72570ff5dee55f42726cc680de4
Nathan Kerr <nathan@manifestwebdesign.com>
2014-07-29 17:43:56 -0700

    Changing the template

--- test/generator/templates/base_model_generator.dart
+++ test/generator/templates/base_model_generator.dart
@@ -1,15 +1,17 @@
-import '../../../lib/dabl.dart';
-import '../../../lib/string_format.dart';
+import 'package:dabl/dabl.dart';
+import 'package:dabl/string_format.dart';
+import 'package:dabl/dbmanager.dart' as DBManager;
 import '../../../packages/unittest/unittest.dart';
 import 'dart:io';
+import 'package:dabl/generator/generator.dart';
 
 main() {
    	Map<String, String> conn_params = {
    		'driver': 'mysql',
    		'host': '127.0.0.1',
-   		'dbname': 'hoffman_ssm',
-   		'user': 'hoffman',
-   		'password': 'G00bers!'
+   		'dbname': 'people',
+   		'user': 'root',
+   		'password': 'password'
    	};
    	DBManager.addConnection('hoffman', conn_params);
    	DefaultGenerator dg = new DefaultGenerator('hoffman');
